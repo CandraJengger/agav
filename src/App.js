caa3bb21781ce5a6d2e102cf348d6dd9e5c95402
CandraJengger <cakjengger@gmail.com>
2021-06-10 13:08:48 +0700

    Base project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Gap } from './components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Halo Dunia</h1>
+      <Button text="Generate" />
+      <Gap width="2px" height="50px" />
+      <Button disabled text="Disabled" />
     </div>
   );
 }
