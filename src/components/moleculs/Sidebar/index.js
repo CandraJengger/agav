186242b9e4fa707d1295c4beee016216505e737d
CandraJengger <cakjengger@gmail.com>
2021-06-14 11:09:34 +0700

    Responsive Layouts

---
+++ src/components/moleculs/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from '..';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/components/organisms/AppLayout/index.js
+++ src/components/organisms/AppLayout/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import Hidden from '@material-ui/core/Hidden';
+import withWidth from '@material-ui/core/withWidth';
 import { MainContainer } from '../../atom';
-import { Navbar } from '../../moleculs';
+import { Navbar, Sidebar } from '../../moleculs';
+import Grid from '@material-ui/core/Grid';
 
 const AppLayout = ({ children }) => {
   return (
     <>
-      <Navbar />
-      <MainContainer>{children}</MainContainer>
+      <Hidden mdUp>
+        <Navbar />
+      </Hidden>
+      <Grid container spacing="3">
+        <Grid item md={4}>
+          <Hidden smDown>
+            <Sidebar />
+          </Hidden>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <MainContainer>{children}</MainContainer>
+        </Grid>
+      </Grid>
     </>
   );
 };
 
-export default AppLayout;
+export default withWidth()(AppLayout);
